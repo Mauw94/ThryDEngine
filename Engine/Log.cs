cdb8fce9c77c32896fef4ec24b4033b6ba233e7a
Maurits Seelen <mauritsseelen@gmail.com>
2022-04-25 14:48:39 +0200

    write log to file

--- Engine/Log.cs
+++ Engine/Log.cs
@@ -7,30 +7,52 @@
     {
         public static void Normal(string msg)
         {
+            var message = $"[MSG] - {msg}";
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"[MSG] - {msg}");
+            Console.WriteLine(message);
             Console.ForegroundColor = ConsoleColor.White;
+
+            WriteToFile(message);
         }
 
         public static void Info(string msg)
         {
+            var message = $"[INFO] - {msg}";
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"[INFO] - {msg}");
+            Console.WriteLine(message);
             Console.ForegroundColor = ConsoleColor.White;
+
+            WriteToFile(message);
         }
 
         public static void Warning(string msg)
         {
+            var message = $"[WARNING] - {msg}";
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"[WARNING] - {msg}");
+            Console.WriteLine(message);
             Console.ForegroundColor = ConsoleColor.White;
+
+            WriteToFile(message);
         }
 
         public static void Error(string msg)
         {
+            var message = $"[ERROR] - {msg}";
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"[ERROR] - {msg}");
+            Console.WriteLine(message);
             Console.ForegroundColor = ConsoleColor.White;
+
+            WriteToFile(message);
+        }
+
+        private static void WriteToFile(string msg)
+        {
+            msg += $"\t{DateTime.Now}";
+
+            Directory.CreateDirectory("Logs");
+
+            using StreamWriter file = new("Logs/log.txt", append: true);
+            file.WriteLine(msg);
         }
     }
 }
